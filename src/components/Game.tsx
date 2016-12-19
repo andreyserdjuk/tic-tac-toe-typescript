773d3e93d4b93e404983c777e716cc8c8924c08a
andreyserdjuk <ase6120@gmail.com>
2016-12-19 22:53:46 +0200

    go over history

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -2,7 +2,8 @@ import * as React from "react";
 import {Board} from "./Board";
 
 export interface GameState {
-    history: Array<{squares:Array<string|null>, xIsNext:boolean}>
+    history: Array<{squares: Array<string|null>, xIsNext: boolean}>,
+    historyStep: number,
 }
 
 export class Game extends React.Component<undefined, GameState> {
@@ -24,13 +25,15 @@ export class Game extends React.Component<undefined, GameState> {
         squares: Array(9).fill(null),
         xIsNext: true,
       }],
+      historyStep: 0,
     };
   }
 
   handleClick(i:number) {
     console.log(i);
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const historyStep = this.state.historyStep;
+    const current = history[historyStep];
     const squares = current.squares.slice();
     squares[i] = current.xIsNext? 'X' : 'O';
 
@@ -45,7 +48,8 @@ export class Game extends React.Component<undefined, GameState> {
       history: this.state.history.concat({
           squares: squares,
           xIsNext: !current.xIsNext,
-      })
+      }),
+      historyStep: this.state.historyStep + 1
     });
   }
 
@@ -54,13 +58,34 @@ export class Game extends React.Component<undefined, GameState> {
     return wonLine? s[wonLine[0]] : false;
   }
 
+  jumpTo(step:number) {
+    this.setState({
+      history: this.state.history.slice(),
+      historyStep: step,
+    });
+  }
+
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const historyStep = this.state.historyStep;
+    const current = history[historyStep];
     const winner = Game.calculateWinner(current.squares);
 
     let nextPlayer = current.xIsNext? 'X' : 'O';
     let status = winner? `Winner: ${winner}` : `Next player: ${nextPlayer}`
+
+    const moves = history.map((step, stepIndex) => {
+      const desc = stepIndex
+        ? `Move #${stepIndex}`
+        : `Game start`
+      ;
+
+      return(
+        <li key={stepIndex}>
+          <a href="#" onClick={() => this.jumpTo(stepIndex)}>{desc}</a>
+        </li>
+      );
+    });
 
     return (
       <div className="game">
@@ -72,7 +97,7 @@ export class Game extends React.Component<undefined, GameState> {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
