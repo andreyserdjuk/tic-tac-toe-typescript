7065800b0435fe97ab8b7507262865815bee5fe3
andreyserdjuk <ase6120@gmail.com>
2017-06-26 18:20:53 +0300

    webpack 3 support, updated tsconfig

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,49 @@
-module.exports = {
-    entry: "./src/index.tsx",
-    output: {
-        filename: "bundle.js",
-        path: __dirname + "/dist"
-    },
+const path = require('path');
 
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
+module.exports = env => {
+    const config = {
+        entry: "./src/index.tsx",
+        output: {
+            filename: "bundle.js",
+            path: path.resolve(__dirname, "dist"),
+        },
 
-    resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
-    },
+        devtool: "source-map",
 
-    module: {
-        loaders: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader" }
-        ],
+        resolve: {
+            extensions: [".ts", ".tsx", ".js"]
+        },
 
-        preLoaders: [
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { test: /\.js$/, loader: "source-map-loader" }
-        ]
-    },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: {
+                        loader: "awesome-typescript-loader"
+                    }
+                },
+                {
+                    test: /\.js$/,
+                    loader: "source-map-loader",
+                    // enforce: "pre"
+                }
+            ]
+        },
 
-    // When importing a module whose path matches one of the following, just
-    // assume a corresponding global variable exists and use that instead.
-    // This is important because it allows us to avoid bundling all of our
-    // dependencies, which allows browsers to cache those libraries between builds.
-    externals: {
-        "react": "React",
-        "react-dom": "ReactDOM"
-    },
+        // When importing a module whose path matches one of the following, just
+        // assume a corresponding global variable exists and use that instead.
+        // This is important because it allows us to avoid bundling all of our
+        // dependencies, which allows browsers to cache those libraries between builds.
+        externals: {
+            "react": "React",
+            "react-dom": "ReactDOM"
+        },
+    };
+
+    if (typeof env !== 'undefined' && env.debug) {
+        console.log(env);
+        debugger;
+    }
+
+    return config;
 };
